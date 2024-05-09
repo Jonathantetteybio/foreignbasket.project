22cdc3b3b52326303254587a9b67bcb9c6c6112a
Jonathantetteybio <jonathantetteybio@gmail.com>
2024-05-09 14:31:56 -0700

    modified axios.js

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'foreignbasket-project-backend-dfyycxbgn.vercel.app',
+  baseURL: 'https://foreignbasket-project-backend-c1u2irmol.vercel.app/',
 });
 
 export default instance;
